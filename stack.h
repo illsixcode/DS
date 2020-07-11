219dad747b8af218534050fcdb14ba5be6834ec6
illsixcode <67185976+illsixcode@users.noreply.github.com>
2020-07-11 18:02:38 +0900

    stack

---
+++ stack.h
@@ -0,0 +1,67 @@
+/*
+11.07.2020
+HYUN JAE CHO
+DATA STRUCTURE - STACK
+*/
+#include "Node.h"
+
+//stack
+//push
+//pop
+//peek
+
+template <typename T>
+class Stack
+{
+public:
+    Stack()
+    {
+        head = top = nullptr;
+    }
+    void Push(T param)
+    {
+        if (head == nullptr)
+        {
+            head = top = new Node<T>(param);
+        }
+        else
+        {
+            Node<T> *newNode = new Node<T>(param);
+            top->next = newNode;
+            top = newNode;
+        }
+    }
+
+    T Pop()
+    {
+        if (head == nullptr)
+        {
+            T err;
+            return err;
+        }
+        else
+        {
+            T value = top->getValue();
+            Node<T> *cur = head;
+            Node<T> *prev = nullptr;
+            while (cur != top)
+            {
+                prev = cur;
+                cur = cur->next;
+            }
+            prev->next = nullptr;
+            delete top;
+            top = prev;
+            return value;
+        }
+    }
+
+    T Peek()
+    {
+        return top->getValue();
+    }
+
+private:
+    Node<T> *head;
+    Node<T> *top;
+};
